a00bb0215dcaad3e24e80b3b7e30afad37dc7182
m.domasevich@andersenlab.com <m.domasevich@andersenlab.com>
2017-08-22 12:55:16 +0300

    Added textarea, button, .click & alert

---
+++ dist/js/index.js
@@ -0,0 +1,9 @@
+"use strict";
+$(document).ready( function(){
+	$(document).ready(function() {
+    $("#add").click(function(e) {
+        e.preventDefault();
+        alert("Form submitted");
+    });
+});
+})
