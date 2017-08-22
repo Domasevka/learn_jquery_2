e5292c7e660771746cb2f2a8a125c562a6961085
m.domasevich@andersenlab.com <m.domasevich@andersenlab.com>
2017-08-22 18:35:20 +0300

    added input

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,9 +1,26 @@
 "use strict";
-$(document).ready( function(){
-	$(document).ready(function() {
+$(document).ready( function(){	
+
     $("#add").click(function(e) {
         e.preventDefault();
         alert("Form submitted");
     });
+	
+	$(function() {
+		
+    $("#submitForm").validate();
+
+    $("#submitForm").submit(function(){                       
+        if ($("#submitForm").valid()){
+            $.post("/create/post/",
+                   {'data':$('#mytextarea').val()},
+                   function(data){
+                       console.log(data);
+                       alert(data);
+                   });
+        }
+        return false;
+    });
+ })
+
 });
-})
