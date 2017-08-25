5865b30783a3b43a5d4bb80f143f8355e38bc87f
m.domasevich@andersenlab.com <m.domasevich@andersenlab.com>
2017-08-25 16:25:06 +0300

    added messages

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,26 +1,38 @@
 "use strict";
-$(document).ready( function(){	
+
+$(document).ready( function(){
 
 
    $('input[value="Submit text"]').click(function() {
 
-        var str = $('textarea:first').serialize();
+        var str = $('textarea.form-control').serialize();
 
-       $("<p/>").text(str).appendTo($('div.messages_list'));
+        /*var message = $("<p/>")
+            .addClass('alert alert-primary')
+            .text(str);
+            .appendTo($('div.messages-list'));
+
+        var now = $("<span/>")
+            .addClass('messages-list__date')
+            .appendTo($('p.alert alert-primary'));*/
+
+         var dNow = new Date();
+         var localdate = (dNow.getMonth()+1) + '/' + dNow.getDate() + '/' + dNow.getFullYear() + ' ' + dNow.getHours() + ':' + dNow.getMinutes();
+         $('span.messages-list__date').text(localdate);
+
+
+        var custom_message = "<div class=\"alert alert-primary\" role=\"alert\"><p>" + str + "</p><span>" + localdate + "</span></div>";
+        $("div.messages-list").append(custom_message);
+
         $( "form" ).submit(function( event ) {
             event.preventDefault();
         });
+
+       $( '#auth' ).each(function(){
+           this.reset();
+       });
+
     });
+});
 
-    /*function showValues(){
 
-        var str = $( 'form' ).serialize();
-         $( 'div.messages_list' ).text( str );
-    }
-
-        $( "textarea[name='comment']" ).on( "click", showValues );
-        //$( 'input[value="Submit text"]' ).on( "click", showValues );
-        $( "textarea" ).on( "change", showValues );
-    showValues();*/
-
-})
