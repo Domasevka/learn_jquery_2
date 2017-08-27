ad276ebcff0b32dd48da820522b272fb7b5301d3
Marina <mdomasevich@gmail.com>
2017-08-27 13:45:40 +0300

    added ajax file

--- dist/js/index.js
+++ dist/js/index.js
@@ -20,9 +20,28 @@ $(document).ready( function(){
          var localdate = (dNow.getMonth()+1) + '/' + dNow.getDate() + '/' + dNow.getFullYear() + ' ' + dNow.getHours() + ':' + dNow.getMinutes();
          $('span.messages-list__date').text(localdate);
 
+       $.getJSON( "ajax/test.json", function( data ) {
+           console.log(data);
 
-        var custom_message = "<div class=\"alert alert-primary\" role=\"alert\"><p>" + str + "</p><span>" + localdate + "</span></div>";
-        $("div.messages-list").append(custom_message);
+
+           if (data && data.title && data.answers) {
+               var title  = $('<h4>').html(data.title).addClass('question-block__name');
+               $('.question-block').append(title);
+
+               var items = [];
+               $.each( data.answers, function( key, val ) {
+                   items.push( "<li class='ellipse__item'>" + val + "</li>" );
+               });
+
+               $( "<ul/>", {
+                   "class": "ellipse",
+                   html: items.join( "" )
+               }).appendTo( ".question-block" );
+           }
+       });
+
+       /* var custom_message = "<div class=\"alert alert-primary\" role=\"alert\"><p>" + str + "</p><span>" + localdate + "</span></div>";
+        $("div.messages-list").append(custom_message);*/
 
         $( "form" ).submit(function( event ) {
             event.preventDefault();
@@ -35,4 +54,26 @@ $(document).ready( function(){
     });
 });
 
+/*$(function() {
 
+    $.getJSON( "ajax/test.json", function( data ) {
+        console.log(data);
+
+
+        if (data && data.title && data.answers) {
+            var title  = $('<h4>').html(data.title).addClass('question-block__name');
+            $('.question-block').append(title);
+
+            var items = [];
+            $.each( data.answers, function( key, val ) {
+                items.push( "<li class='ellipse__item'>" + val + "</li>" );
+            });
+
+            $( "<ul/>", {
+                "class": "ellipse",
+                html: items.join( "" )
+            }).appendTo( ".question-block" );
+        }
+    });
+
+});*/
