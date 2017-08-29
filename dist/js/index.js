1484c84616aa56656f08aac13de40b419c8fe1ed
m.domasevich@andersenlab.com <m.domasevich@andersenlab.com>
2017-08-29 10:12:03 +0300

    adding new function

--- dist/js/index.js
+++ dist/js/index.js
@@ -20,24 +20,9 @@ $(document).ready( function(){
          var localdate = (dNow.getMonth()+1) + '/' + dNow.getDate() + '/' + dNow.getFullYear() + ' ' + dNow.getHours() + ':' + dNow.getMinutes();
          $('span.messages-list__date').text(localdate);
 
-       $.getJSON( "ajax/test.json", function( data ) {
-           console.log(data);
-
-
-           if (data && data.title && data.answers) {
-               var title  = $('<h4>').html(data.title).addClass('question-block__name');
-               $('.question-block').append(title);
-
-               var items = [];
-               $.each( data.answers, function( key, val ) {
-                   items.push( "<li class='ellipse__item'>" + val + "</li>" );
-               });
-
-               $( "<ul/>", {
-                   "class": "ellipse",
-                   html: items.join( "" )
-               }).appendTo( ".question-block" );
-           }
+       jQuery.getJSON( "index.json", function( data ) {
+           //console.log(data.kakoetoslovo);
+           $('div.messages-list').append(data.kakoetoslovo);
        });
 
        /* var custom_message = "<div class=\"alert alert-primary\" role=\"alert\"><p>" + str + "</p><span>" + localdate + "</span></div>";
